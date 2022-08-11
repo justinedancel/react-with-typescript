23d759db63bf207e1bfcdfd9e3cf1395eb5a750b
Justine Dancel <justine.dancel204@gmail.com>
2022-08-10 21:28:07 -0500

    Handling state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IState {
+    people: {
+        name: string,
+        age: number,
+        imageUrl: string,
+        note?: string, // ? - optional
+    }[] // [] - going to be an array
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [people, setPeople] = useState<IState["people"]>([])
+
+    return (
+        <div className="App">
+            <h1>Some pretty okay baseball players</h1>
+        </div>
+    );
 }
 
 export default App;
