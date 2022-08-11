0b51331895b310e9d42ec129496214f67620dc1b
Justine Dancel <justine.dancel204@gmail.com>
2022-08-10 21:54:08 -0500

    Handling functions

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,26 @@ interface IState {
 }
 
 function App() {
-    const [people, setPeople] = useState<IState["people"]>([])
+    const [people, setPeople] = useState<IState["people"]>([
+        {
+            name: "Shohei Ohtani",
+            age: 28,
+            imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/39832.png",
+            note: "Two-way player",
+        },
+        {
+            name: "Mike Trout",
+            age: 31,
+            imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30836.png&w=350&h=254",
+            note: "The GOAT",
+        },
+        {
+            name: "Ichiro Suzuki",
+            age: 48,
+            imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/4570.png",
+            note: "Area 51 cannon",
+        }
+    ])
 
     return (
         <div className="App">
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,10 +12,23 @@ interface IProps {
 
 const List: React.FC<IProps> = ({ people }) => {
 
+    const renderList = (): JSX.Element[] => {
+        return people.map((person) => (
+            <li className="list-item">
+                <div className="list-header">
+                    <img className="list-image" alt={`${person.name}-imageUrl`} src={person.imageUrl} />
+                    <h2>{person.name}</h2>
+                </div>
+                <p>{person.age} years old</p>
+                <p className="list-note">{person.note}</p>
+            </li>
+        ))
+    }
+
     return (
-        <div>
-            List, I am
-        </div>
+        <ul>
+            {renderList()}
+        </ul>
     );
 }
 
